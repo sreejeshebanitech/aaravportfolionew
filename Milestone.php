9c0edbca1d4788b5a81439fe5f8175a1108c7451
sreejeshebanitech <sreejesh@ebanitech.com>
2026-02-22 01:26:57 +0530

    Added Milestone

---
+++ Milestone.php
@@ -0,0 +1,31 @@
+<?php
+$meta_title = "Aarav Jadon Puttanam | Aspiring Software Developer";
+$meta_description = "Official portfolio of Aarav Jadon Puttanam, Cambridge Grade 12 student passionate about AI, business, and innovation.";
+$meta_keywords = "Aarav Jadon Puttanam, AI student, Cambridge student, aspiring developer";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <?php include('./globals/meta.php'); ?>
+
+    <!-- Global CSS -->
+    <link rel="stylesheet" href="./globals/headerInsidePage.css">
+
+    <!-- Section CSS -->
+    <link rel="stylesheet" href="./pages/HomePage/HomePage.css">
+    <link rel="stylesheet" href="./pages/HomePage/PetxShow/PetxGallary.css">
+
+  
+
+</head>
+<body>
+
+    <?php include('./globals/header.php'); ?>
+     <?php include('./pages/HomePage/PetxShow/PetxPage.php'); ?>
+
+  
+
+</body>
+</html>
