3ab2c982c1925e7b79cc27090c872d0c20782cd6
sreejeshebanitech <sreejesh@ebanitech.com>
2026-02-21 22:33:02 +0530

    Added AIR Content

---
+++ Pages/HomePage/AIR/AIR.php
@@ -0,0 +1,35 @@
+<section class="radio-section">
+  <div class="radio-container">
+    
+    <div class="radio-top-grid">
+      <div class="radio-text-top">
+        <h2 class="radio-title">All India Radio Interview <br> – A Special Moment</h2>
+        <p>
+          I had the opportunity to give my first live interview on All India Radio, one of India’s most widely listened-to radio networks. During the interview, I spoke about my coding journey, building Vet Bookr, my passion for badminton, and how I balanced everything alongside academics. It was a new and exciting experience to share my story with a larger audience.
+        </p>
+      </div>
+      <div class="radio-img-large">
+        <img src="./Assets/ImageAssets/Radio/Studio_Wide.jpg" alt="Live Interview Studio">
+      </div>
+    </div>
+
+    <div class="radio-bottom-grid">
+      
+      <div class="radio-col-1">
+        <img src="./Assets/ImageAssets/Radio/Entrance.jpg" alt="AIR Entrance">
+      </div>
+
+      <div class="radio-col-2">
+        <img class="img-script" src="./Assets/ImageAssets/Radio/Script_Standing.jpg" alt="Standing with Scripts">
+        <img class="img-team" src="./Assets/ImageAssets/Radio/Team_Smile.jpg" alt="With the Interviewer">
+      </div>
+
+      <div class="radio-col-3">
+        <p class="reflection-text">
+          I learned how important it is to communicate clearly and honestly. What meant the most to me was when the host mentioned that my journey could inspire other students listening - that moment truly stayed with me.
+        </p>
+      </div>
+      
+    </div>
+  </div>
+</section>
