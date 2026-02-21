9b199a1fa73073cb61038a666b5007fd6c7aaef4
sreejeshebanitech <sreejesh@ebanitech.com>
2026-02-21 12:50:32 +0530

    dded

--- index.php
+++ index.php
@@ -2,13 +2,23 @@
 $meta_title = "Aarav Jadon Puttanam | Aspiring Software Developer";
 $meta_description = "Official portfolio of Aarav Jadon Puttanam, Cambridge Grade 12 student passionate about AI, business, and real-world innovation.";
 $meta_keywords = "Aarav Jadon Puttanam, AI student, Cambridge student, aspiring software developer";
-
-include('./globals/meta.php');
-include('./globals/header.php');
-include('./pages/HomePage/homePage.php');
-// include('./pages/HomePage/TechPage/techPage.php');
-
-
 ?>
 
-<link rel="stylesheet" href="./globals/header.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <?php include('./globals/meta.php'); ?>
+
+    <!-- Global Styles -->
+    <link rel="stylesheet" href="./globals/header.css">
+    <link rel="stylesheet" href="./pages/HomePage/HomePage.css">
+
+</head>
+<body>
+
+    <?php include('./globals/header.php'); ?>
+    <?php include('./pages/HomePage/homePage.php'); ?>
+
+</body>
+</html>
