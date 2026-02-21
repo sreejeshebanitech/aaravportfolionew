551c2de348329f9ebdbda4b55517d3e2b53525a7
sreejeshebanitech <sreejesh@ebanitech.com>
2026-02-21 13:46:29 +0530

    Added Change

---
+++ vetbooker.php
@@ -0,0 +1,28 @@
+<?php
+$meta_title = "Aarav Jadon Puttanam | Aspiring Software Developer";
+$meta_description = "Official portfolio of Aarav Jadon Puttanam, Cambridge Grade 12 student passionate about AI, business, and innovation.";
+$meta_keywords = "Aarav Jadon Puttanam, AI student, Cambridge student, aspiring developer";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <?php include('./globals/meta.php'); ?>
+
+    <!-- Global CSS -->
+    <link rel="stylesheet" href="./globals/header.css">
+
+    <!-- Section CSS -->
+    <link rel="stylesheet" href="./pages/HomePage/HomePage.css">
+    <link rel="stylesheet" href="./pages/HomePage/TechPage/techPage.css">
+
+</head>
+<body>
+
+    <?php include('./globals/header.php'); ?>
+
+  
+
+</body>
+</html>
